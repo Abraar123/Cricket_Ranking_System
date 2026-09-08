d194f8b603a84a72528a6edd851f7bcae10711d6
demo <abraarahamed39@gmail.com>
2026-09-08 18:47:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/cric/cricket_ranking_system/CricketRankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cric.cricket_ranking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketRankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketRankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cric/cricket_ranking_system/CricketRankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cric.cricket_ranking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketRankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
